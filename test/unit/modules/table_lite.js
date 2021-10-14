c67459ee9593f0dd213506cd935d6835bfe49e5a
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2021-10-14 15:51:04 +0300

    import preset

--- test/unit/modules/table_lite.js
+++ test/unit/modules/table_lite.js
@@ -2,54 +2,10 @@ import Delta from 'quill-delta';
 import Quill from '../../../core/quill';
 import TableLite from '../../../modules/table/lite';
 import capitalize from '../../../utils/capitalize';
-
-const TABLE_STYLE_TESTS_PRESET = [
-  { formatName: 'width', styleName: 'width', value: '100px' },
-  { formatName: 'height', styleName: 'height', value: '100px' },
-  { formatName: 'textAlign', styleName: 'text-align', value: 'right' },
-  { formatName: 'border', styleName: 'border', value: '1px solid red' },
-  { formatName: 'borderWidth', styleName: 'border-width', value: '2px' },
-  { formatName: 'borderColor', styleName: 'border-color', value: 'green' },
-  { formatName: 'borderStyle', styleName: 'border-style', value: 'dashed' },
-  {
-    formatName: 'backgroundColor',
-    styleName: 'background-color',
-    value: 'red',
-  },
-];
-
-const CELL_STYLE_TESTS_PRESET = [
-  { formatName: 'width', styleName: 'width', value: '100px' },
-  { formatName: 'height', styleName: 'height', value: '100px' },
-  { formatName: 'border', styleName: 'border', value: '1px solid red' },
-  { formatName: 'borderWidth', styleName: 'border-width', value: '2px' },
-  { formatName: 'borderColor', styleName: 'border-color', value: 'green' },
-  { formatName: 'borderStyle', styleName: 'border-style', value: 'dashed' },
-  {
-    formatName: 'backgroundColor',
-    styleName: 'background-color',
-    value: 'red',
-  },
-  {
-    formatName: 'verticalAlign',
-    styleName: 'vertical-align',
-    value: 'bottom',
-  },
-  {
-    formatName: 'textAlign',
-    styleName: 'text-align',
-    value: 'center',
-  },
-  { formatName: 'padding', styleName: 'padding', value: '20px' },
-  { formatName: 'paddingTop', styleName: 'padding-top', value: '5px' },
-  { formatName: 'paddingLeft', styleName: 'padding-left', value: '5px' },
-  { formatName: 'paddingRight', styleName: 'padding-right', value: '5px' },
-  {
-    formatName: 'paddingBottom',
-    styleName: 'padding-bottom',
-    value: '5px',
-  },
-];
+import {
+  CELL_STYLE_TESTS_PRESET,
+  TABLE_STYLE_TESTS_PRESET,
+} from '../../helpers/table_format_presets';
 
 describe('Table Module', function() {
   beforeAll(function() {
