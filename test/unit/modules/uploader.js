ec4581c3e839bfb3a0baad5e0b0eea8fe0903a9f
CORP\kotov.alexander <alexander.kotov@devexpress.com>
2021-12-16 15:48:47 +0300

    test all supported types

--- test/unit/modules/uploader.js
+++ test/unit/modules/uploader.js
@@ -3,51 +3,66 @@ import { Range } from '../../../core/selection';
 
 describe('Uploader', function() {
   describe('image uploading', function() {
-    it('Check file types upload filtration', function() {
-      const testRange = new Range(0);
-      let uploads;
-
-      const quillMock = {
-        root: {
-          addEventListener: () => {},
-        },
-      };
-
-      const gifFile = {
-        name: 'test.gif',
-        type: 'image/gif',
-      };
-
-      const pngFile = {
+    [
+      {
         name: 'test.png',
         type: 'image/png',
-      };
-
-      const htmlFile = {
-        name: 'test.html',
-        type: 'text/html',
-      };
-
-      const iconFile = {
+      },
+      {
+        name: 'test.jpeg',
+        type: 'image/jpeg',
+      },
+      {
+        name: 'test.pjpeg',
+        type: 'image/pjpeg',
+      },
+      {
+        name: 'test.gif',
+        type: 'image/gif',
+      },
+      {
+        name: 'test.webp',
+        type: 'image/webp',
+      },
+      {
+        name: 'test.bmp',
+        type: 'image/bmp',
+      },
+      {
+        name: 'test.svg',
+        type: 'image/svg+xml',
+      },
+      {
         name: 'test.icon',
         type: 'image/vnd.microsoft.icon',
-      };
+      },
+      {
+        name: 'test.html',
+        type: 'text/html',
+        isNotImage: true,
+      },
+    ].forEach(file => {
+      it(`upload ${file.name}`, function() {
+        const testRange = new Range(0);
+        let uploads = [];
 
-      const uploaderInstance = new Uploader(quillMock, {
-        mimetypes: Uploader.DEFAULTS.mimetypes,
-        handler: (range, files) => {
-          uploads = files;
-        },
+        const quillMock = {
+          root: {
+            addEventListener: () => {},
+          },
+        };
+
+        const uploaderInstance = new Uploader(quillMock, {
+          mimetypes: Uploader.DEFAULTS.mimetypes,
+          handler: (range, files) => {
+            uploads = files;
+          },
+        });
+
+        uploaderInstance.upload(testRange, [file]);
+
+        expect(uploads.length).toEqual(file.isNotImage ? 0 : 1);
       });
-
-      uploaderInstance.upload(testRange, [
-        gifFile,
-        pngFile,
-        htmlFile,
-        iconFile,
-      ]);
-
-      expect(uploads).toEqual([gifFile, pngFile, iconFile]);
     });
   });
 });
