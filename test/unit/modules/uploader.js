2af0af01682686a3bf079e291df68a2b0decd57f
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2021-12-23 11:37:03 +0300

    Fix inability to drag and drop text content into the editor (T1037479) (#29)
    * update core-js
    
    * Support  more image mimetypes for image uploader (T1049637) (#27)
    
    (cherry picked from commit bc95706a424850c73fe93ecfa05c2a707774fbd0)
    
    * Fix inability to drag and drop text content into the editor
    
    (cherry picked from commit fde3a8e4f0a62d82a7c6918a13e6944b7aacd747)
    
    * Update CI
    
    * update test
    
    Co-authored-by: kotov.alexander <alexander.kotov@devexpress.com>

---
+++ test/unit/modules/uploader.js
@@ -0,0 +1,112 @@
+import Uploader from '../../../modules/uploader';
+import { Range } from '../../../core/selection';
+
+describe('Uploader', function() {
+  describe('image uploading', function() {
+    [
+      {
+        name: 'test.png',
+        type: 'image/png',
+      },
+      {
+        name: 'test.jpeg',
+        type: 'image/jpeg',
+      },
+      {
+        name: 'test.pjpeg',
+        type: 'image/pjpeg',
+      },
+      {
+        name: 'test.gif',
+        type: 'image/gif',
+      },
+      {
+        name: 'test.webp',
+        type: 'image/webp',
+      },
+      {
+        name: 'test.bmp',
+        type: 'image/bmp',
+      },
+      {
+        name: 'test.svg',
+        type: 'image/svg+xml',
+      },
+      {
+        name: 'test.icon',
+        type: 'image/vnd.microsoft.icon',
+      },
+      {
+        name: 'test.html',
+        type: 'text/html',
+        isNotImage: true,
+      },
+    ].forEach(file => {
+      it(`upload ${file.name}`, function() {
+        const testRange = new Range(0);
+        let uploads = [];
+
+        const quillMock = {
+          root: {
+            addEventListener: () => {},
+          },
+        };
+
+        const uploaderInstance = new Uploader(quillMock, {
+          mimetypes: Uploader.DEFAULTS.mimetypes,
+          handler: (range, files) => {
+            uploads = files;
+          },
+        });
+
+        uploaderInstance.upload(testRange, [file]);
+
+        expect(uploads.length).toEqual(file.isNotImage ? 0 : 1);
+      });
+    });
+
+    it('should not prevent default when no files to drop', function() {
+      const quillMock = {
+        root: document.createElement('input'),
+      };
+
+      // eslint-disable-next-line no-new
+      new Uploader(quillMock);
+      const dataTransferInstance = new DataTransfer();
+      dataTransferInstance.setData('text/plain', 'just text');
+      const dropEvent = new DragEvent('drop', {
+        dataTransfer: dataTransferInstance,
+        cancelable: true,
+      });
+
+      quillMock.root.dispatchEvent(dropEvent);
+
+      expect(dropEvent.defaultPrevented).toBeFalse();
+    });
+
+    it('should prevent default on drop files', function() {
+      const quillMock = {
+        root: document.createElement('input'),
+      };
+
+      // eslint-disable-next-line no-new
+      new Uploader(quillMock);
+      const dataTransferInstance = new DataTransfer();
+      const fileContent = ['<u>test</u>'];
+
+      dataTransferInstance.setData('text/plain', 'just text');
+      dataTransferInstance.items.add(
+        new File([new Blob(fileContent, { type: 'text/html' })], 'test.html'),
+      );
+
+      const dropEvent = new DragEvent('drop', {
+        dataTransfer: dataTransferInstance,
+        cancelable: true,
+      });
+
+      quillMock.root.dispatchEvent(dropEvent);
+
+      expect(dropEvent.defaultPrevented).toBeTrue();
+    });
+  });
+});
