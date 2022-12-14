92dac8262181500ef5c16d124a8cd1305d0565cd
Ilya Vinogradov <48182348+williamvinogradov@users.noreply.github.com>
2022-12-14 23:08:31 +0700

    T1113606: Fix table attributors keyNames collision. (#71)
    * Quill 1.5: Fix strikethrough attributes issue.
    
    * Quill 1.5: Code review fixes [0].
    
    * Quill 1.5: Fix keyNames collision.
    
    * Quill 1.5: Rework of the table's keyNames split mechanism.
    
    Also, add some tests + code review fixes.
    
    * Quill 1.5: Add fix for DOM attributes.
    
    Also, remove redundant class attributor
    + code review fixes
    + some little structure refactoring.
    
    Co-authored-by: Ilya Vinogradov <ilya.vinogradov@devexpress.com>

--- formats/table/attributors/cell.js
+++ formats/table/attributors/cell.js
@@ -1,125 +1,31 @@
 import prepareAttributor from './prepare_attributor';
 import prepareStyleAttributor from './prepare_style_attributor';
-
-const cellConfig = {
-  name: 'cell',
-  allowedTags: ['TH', 'TD'],
-};
-
-const CellHeightAttribute = prepareAttributor(cellConfig, 'height');
-const CellWidthAttribute = prepareAttributor(cellConfig, 'width');
-const CellHeightStyle = prepareStyleAttributor(cellConfig, 'height');
-const CellWidthStyle = prepareStyleAttributor(cellConfig, 'width');
-
-const CellVerticalAlignStyle = prepareStyleAttributor(
+import {
   cellConfig,
-  'vertical',
-  'align',
+  TABLE_CELL_ATTRIBUTES,
+  TABLE_CELL_STYLES,
+} from './cell_config';
+
+export const TABLE_CELL_ATTR_ATTRIBUTORS = TABLE_CELL_ATTRIBUTES.map(attrName =>
+  prepareAttributor(cellConfig, attrName),
 );
 
-const CellTextAlignStyle = prepareStyleAttributor(cellConfig, 'text', 'align');
-
-const CellBackgroundColorStyle = prepareStyleAttributor(
-  cellConfig,
-  'background',
-  'color',
+export const TABLE_CELL_STYLE_ATTRIBUTORS = TABLE_CELL_STYLES.map(styleName =>
+  prepareStyleAttributor(cellConfig, styleName),
 );
 
-const CellBorderStyle = prepareStyleAttributor(cellConfig, 'border');
-const CellBorderStyleStyle = prepareStyleAttributor(
-  cellConfig,
-  'border',
-  'style',
-);
-const CellBorderWidthStyle = prepareStyleAttributor(
-  cellConfig,
-  'border',
-  'width',
-);
-const CellBorderColorStyle = prepareStyleAttributor(
-  cellConfig,
-  'border',
-  'color',
+export const CELL_FORMATS = TABLE_CELL_STYLE_ATTRIBUTORS.reduce(
+  (result, attributor) => {
+    result[attributor.attrName] = attributor;
+    return result;
+  },
+  {},
 );
 
-const CellPaddingStyle = prepareStyleAttributor(cellConfig, 'padding');
-const CellPaddingTopStyle = prepareStyleAttributor(
-  cellConfig,
-  'padding',
-  'top',
-);
-const CellPaddingBottomStyle = prepareStyleAttributor(
-  cellConfig,
-  'padding',
-  'bottom',
-);
-const CellPaddingLeftStyle = prepareStyleAttributor(
-  cellConfig,
-  'padding',
-  'left',
-);
-const CellPaddingRightStyle = prepareStyleAttributor(
-  cellConfig,
-  'padding',
-  'right',
-);
-
-const CELL_FORMATS = {
-  cellBorder: CellBorderStyle,
-  cellBorderStyle: CellBorderStyleStyle,
-  cellBorderWidth: CellBorderWidthStyle,
-  cellBorderColor: CellBorderColorStyle,
-  cellBackgroundColor: CellBackgroundColorStyle,
-  cellPadding: CellPaddingStyle,
-  cellPaddingTop: CellPaddingTopStyle,
-  cellPaddingBottom: CellPaddingBottomStyle,
-  cellPaddingLeft: CellPaddingLeftStyle,
-  cellPaddingRight: CellPaddingRightStyle,
-  cellVerticalAlign: CellVerticalAlignStyle,
-  cellTextAlign: CellTextAlignStyle,
-  cellWidth: CellWidthStyle,
-  cellHeight: CellHeightStyle,
-};
-
-const CELL_ATTRIBUTORS = [
-  CellBackgroundColorStyle,
-  CellBorderColorStyle,
-  CellBorderStyle,
-  CellBorderStyleStyle,
-  CellBorderWidthStyle,
-  CellPaddingBottomStyle,
-  CellPaddingLeftStyle,
-  CellPaddingRightStyle,
-  CellPaddingStyle,
-  CellPaddingTopStyle,
-  CellVerticalAlignStyle,
-  CellTextAlignStyle,
-  CellHeightStyle,
-  CellWidthStyle,
-  CellWidthAttribute,
-  CellHeightAttribute,
-].reduce((memo, attr) => {
-  memo[attr.keyName] = attr;
-  return memo;
+export const CELL_ATTRIBUTORS = [
+  ...TABLE_CELL_ATTR_ATTRIBUTORS,
+  ...TABLE_CELL_STYLE_ATTRIBUTORS,
+].reduce((result, attributor) => {
+  result[attributor.keyName] = attributor;
+  return result;
 }, {});
-
-export {
-  CellVerticalAlignStyle,
-  CellTextAlignStyle,
-  CellBackgroundColorStyle,
-  CellBorderStyle,
-  CellBorderStyleStyle,
-  CellBorderWidthStyle,
-  CellBorderColorStyle,
-  CellPaddingStyle,
-  CellPaddingTopStyle,
-  CellPaddingBottomStyle,
-  CellPaddingLeftStyle,
-  CellPaddingRightStyle,
-  CellHeightStyle,
-  CellWidthStyle,
-  CellHeightAttribute,
-  CellWidthAttribute,
-  CELL_FORMATS,
-  CELL_ATTRIBUTORS,
-};
